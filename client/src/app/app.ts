f4445e800779301097382f306fb9096a9459ff17
Essarhir Tarik <essarhir.t@outlook.com>
2025-06-28 11:13:17 +0200

    Added a generic footer

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'client';
+  title = 'Tracking Import';
 }
