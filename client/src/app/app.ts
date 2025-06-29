2a9dc83d4fa5238987fa4396a417ef94b1871907
Essarhir Tarik <essarhir.t@outlook.com>
2025-06-29 20:28:09 +0200

    Added a navbar cmponent

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ client/src/app/components/navbar/navbar.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.scss'] // corrected 'styleUrl' to 'styleUrls'
+})
+export class Navbar {
+  public currentRoute: string = '';
+
+  constructor(private router: Router) {
+    // this updates on every route change, including initial load
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentRoute = event.urlAfterRedirects;
+      });
+  }
+}
