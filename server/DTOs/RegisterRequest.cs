b4ed62fbb370b09e2723cee7599aa670944470bc
Essarhir Tarik <essarhir.t@outlook.com>
2025-07-27 13:18:14 +0200

    Added a registraion DTO and conditioned the login form

--- server/DTOs/RegisterRequest.cs
+++ server/DTOs/RegisterRequest.cs
@@ -2,8 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace server.DTOs
 {
-    public class LoginRequest
+    public class RegisterRequest
     {
+        [Required]
+        public string Nom { get; set; } = string.Empty;
+
         [Required]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
@@ -11,5 +14,8 @@ namespace server.DTOs
         [Required]
         [MinLength(8)]
         public string MotDePasse { get; set; } = string.Empty;
+
+        [Required]
+        public int RoleId { get; set; }
     }
 }
